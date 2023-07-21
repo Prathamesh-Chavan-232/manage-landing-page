fba0b1c33a87cc698fc86ced9167c68fc540928a
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2023-07-22 04:18:30 +0530

    init: portfolio ui project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Frontend UI practice
-UI Practice projects mainly to learn CSS, Javascript, React / NextJS, TailwindCSS, Framer motion
-Each Branch in this repository is one project.
--- manage-landing-page/src/main.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("hamburger-menu");
-const menu = document.getElementById("mobile-menu");
-
-btn.addEventListener("click", () => {
-	btn.classList.toggle("open");
-	menu.classList.toggle("flex");
-	menu.classList.toggle("hidden");
-});
